27333ba67e3ee1bb266d5ea8d45cc4eac7d371c7
liushixiong <liushixiong@flash.cn>
2020-05-29 17:05:51 +0800

    增加将UI封装到dll中

--- src/showUI/showUI.cpp
+++ src/showUI/showUI.cpp
@@ -1,14 +1,8 @@
-// test_edr_dll.cpp : 定义应用程序的入口点。
+// showUI.cpp : 定义 DLL 应用程序的导出函数。
 //
 
 #include "stdafx.h"
-#include "test_edr_dll.h"
-// #include "edr.h"
-#include "../edrdll/edr.h"
-#include "../../showUI/showUI.h"
-
-#pragma comment(lib, "edrdll.lib")
-#pragma comment(lib, "showui.lib")
+#include "showUI.h"
 
 #define MAX_LOADSTRING 100
 
@@ -22,48 +16,6 @@ ATOM				MyRegisterClass(HINSTANCE hInstance);
 BOOL				InitInstance(HINSTANCE, int);
 LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
-
-int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPTSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
-{
-	UNREFERENCED_PARAMETER(hPrevInstance);
-	UNREFERENCED_PARAMETER(lpCmdLine);
-
- 	// TODO:  在此放置代码。
-	MSG msg;
-	HACCEL hAccelTable;
-
-	// 初始化全局字符串
-	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-	LoadString(hInstance, IDC_TEST_EDR_DLL, szWindowClass, MAX_LOADSTRING);
-	MyRegisterClass(hInstance);
-
-	// 执行应用程序初始化: 
-	if (!InitInstance (hInstance, nCmdShow))
-	{
-		return FALSE;
-	}
-
-	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_TEST_EDR_DLL));
-
-
-	// 主消息循环: 
-	while (GetMessage(&msg, NULL, 0, 0))
-	{
-		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
-	}
-
-	return (int) msg.wParam;
-}
-
-
-
 //
 //  函数:  MyRegisterClass()
 //
@@ -80,12 +32,12 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 	wcex.cbClsExtra		= 0;
 	wcex.cbWndExtra		= 0;
 	wcex.hInstance		= hInstance;
-	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_TEST_EDR_DLL));
-	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wcex.hIcon			= NULL;
+	wcex.hCursor		= NULL;
 	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
-	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_TEST_EDR_DLL);
+	wcex.lpszMenuName	= NULL;
 	wcex.lpszClassName	= szWindowClass;
-	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+	wcex.hIconSm		= NULL;
 
 	return RegisterClassEx(&wcex);
 }
@@ -139,40 +91,18 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	switch (message)
 	{
 	case WM_COMMAND:
-		wmId    = LOWORD(wParam);
-		wmEvent = HIWORD(wParam);
-		// 分析菜单选择: 
-		switch (wmId)
-		{
-		case IDM_ABOUT:
-			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-			break;
-		case IDM_EXIT:
-			DestroyWindow(hWnd);
-			break;
-		default:
-			return DefWindowProc(hWnd, message, wParam, lParam);
-		}
 		break;
 	case WM_PAINT:
-	{
 		hdc = BeginPaint(hWnd, &ps);
 		// TODO:  在此添加任意绘图代码...
-		
-		RECT rect;
-		GetClientRect(hWnd, &rect);
-
-		EdrCenterText(hdc, &rect, 
-			TEXT("This string was displayed by a DLL."));
-
 		EndPaint(hWnd, &ps);
+		break;
+	case WM_LBUTTONDOWN:
+		break;
+	case WM_RBUTTONDOWN:
+	{
 	}
 		break;
-    case WM_LBUTTONUP:
-    {
-        showWindow(NULL);
-    }
-        break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
@@ -200,4 +130,31 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 		break;
 	}
 	return (INT_PTR)FALSE;
+}
+
+EXPORT BOOL CALLBACK showWindow(HINSTANCE hInstance)
+{
+    	// TODO:  在此放置代码。
+	MSG msg;
+	HACCEL hAccelTable;
+
+	// 初始化全局字符串
+    wcscpy_s(szTitle, 100, L"fefe");
+    wcscpy_s(szWindowClass, 100, L"fefe");
+	MyRegisterClass(NULL);
+
+	// 执行应用程序初始化: 
+	if (!InitInstance (hInstance, SW_SHOW))
+	{
+		return FALSE;
+	}
+
+	// 主消息循环: 
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+	}
+
+	return (int) msg.wParam;
 }
